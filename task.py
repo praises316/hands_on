f005912f619c2e76ab9b1bb486b4f813cafea0e0
praises316 <azennadapal@gmail.com>
2025-07-24 15:56:35 +0100

    number one

---
+++ task.py
@@ -0,0 +1,10 @@
+# Task 1: Jos Food and Culture Festival Menu (Modified)
+meals = ["Gwote", "Masa", "Tuwon Acha", "Fura da Nono", "Kunu", "Miyan Kuka"]
+
+kunu  =  meals.insert(4,"Kunu")
+print(meals)
+
+masa = meals.remove(meals[1])
+print(meals)
+
+furaNunu = 
