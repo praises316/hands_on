618faa34218fa840128ab853f11d0bdab0d1ed38
praises316 <azennadapal@gmail.com>
2025-07-24 16:23:58 +0100

    number two done

--- task.py
+++ task.py
@@ -4,7 +4,27 @@ meals = ["Gwote", "Masa", "Tuwon Acha", "Fura da Nono", "Kunu", "Miyan Kuka"]
 kunu  =  meals.insert(4,"Kunu")
 print(meals)
 
-masa = meals.remove(meals[1])
+meals.remove(meals[1])
 print(meals)
 
-furaNunu = 
+meals.remove(meals[3])
+print(meals)
+meals.append("Fura da nono")
+print(meals)
+
+#2: Film Night Prep
+genres = ["Adventure", "Comedy", "Animation", "Fantasy", "Sci-Fi", "Documentary", "Fantasy"]
+print(genres)
+genres.append("Drama")
+print(genres)
+genres.remove(genres[3])
+genres.remove(genres[5])
+print(genres)
+
+print(len(genres))
+
+print(genres[1])
+print(genres[-2])
+
+newGenreList = genres
+print(newGenreList)
